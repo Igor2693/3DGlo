6f4a6b9e973310819888ebb5813963824afa2404
Igor2693 <i.bugaytsov@mail.ru>
2024-09-08 19:36:19 +0300

    выполнение задания 21

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import form from './modules/form'
+import calculator from './modules/calculator'
 
 
 timer('10 september 2024')
 menu()
 modal()
+form()
+calculator()
 
